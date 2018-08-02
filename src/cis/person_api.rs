095a7fa3c0232ccb0965f5fcc23a9b0ffd5abe45
Florian Merz <flomerz@gmail.com>
2018-08-02 08:31:25 +0200

    Start to improve error handling.

--- src/cis/person_api.rs
+++ src/cis/person_api.rs
@@ -1,10 +1,19 @@
-use rocket_contrib::{Json, Value};
-use rocket::State;
 use data::*;
+use rocket::response::status::NotFound;
+use rocket::State;
+use rocket_contrib::{Json, Value};
 
 #[get("/<user_id>")]
-pub fn person_api(user_id: String, ctx: State<Ctx>) -> Json<Value> {
-    let profile = ctx.ctx.lock().unwrap().get(&user_id).unwrap().clone();
-    Json(profile)
+pub fn person_api(user_id: String, ctx: State<Ctx>) -> Result<Json, NotFound<Json>> {
+    ctx.ctx
+        .lock()
+        .map_err(|e| format!("{}", e))
+        .and_then(|profiles| {
+            profiles
+                .get(&user_id)
+                .map(|p| p.clone())
+                .ok_or_else(|| "profile not found".to_owned())
+        })
+        .map(Json)
+        .map_err(|e| NotFound(Json(json!({ "error": e }))))
 }
-
